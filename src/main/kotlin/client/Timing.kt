d79c93cc078203b0d40713830e02ab1cb1e3ee39
Petr Fuka <petr.fuka@gmail.com>
2024-11-19 02:43:57 +0100

    Fix subtitle timing for split sentences

--- src/main/kotlin/client/Timing.kt
+++ src/main/kotlin/client/Timing.kt
@@ -20,7 +20,10 @@ fun getTimings(speeches: List<SpeechPart>, ssml: String, boundaries: List<Bounda
             index = ssml.indexOf(xmlSentence, index + 1)
 
             val start = boundaries.firstOrNull { it.textOffset == index }?.offset ?: wipTiming?.start
-            val end = boundaries.firstOrNull { it.textOffset == index + xmlSentence.length - 1 }?.endOffset
+            val end = boundaries.firstOrNull {
+                it.textOffset == index + xmlSentence.length - 1
+                        || it.textOffset + it.text.length == index + xmlSentence.length
+            }?.endOffset
 
             val timing = wipTiming.join(Timing(speaker, sentence, start ?: 0.milliseconds, end ?: 0.milliseconds))
             wipTiming = null
